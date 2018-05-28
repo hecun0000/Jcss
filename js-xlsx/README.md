cf3f57354fb4c92363c7f8dc9b0443c364be06dc
JiangYuan <jiangyuan@code.ledouya.com>
2018-05-28 17:25:53 +0800

    上传

--- js-xlsx/README.md
+++ js-xlsx/README.md
@@ -5,3 +5,14 @@
 
 [在线预览excel导出](http://hecun0000.github.io/Jcss/js-xlsx/export.html)
 
+## 利用a标签实现简单的下载
+
+[在线预览a_标签实现下载功能](http://hecun0000.github.io/Jcss/js-xlsx/a_download.html)
+
+
+
+## 显示用户本地选取的图片
+
+
+
+
---
+++ vue-login-toggle/src/components/login.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="account">
+        <div class="container">
+            <div class="content">
+                <h2 class="title">登录</h2>    
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            toForget:false,
+            toForgetCLass:'hide',
+            blockStatus:false
+        }
+    },
+    methods:{
+        forget(){
+            
+        }
+    }
+}
+</script>
+
+
+
+<style scoped>
+.account{
+    width: 100%;
+    height:100%;
+    background: #B9887D;
+    position: absolute;
+}
+.account .container {
+    position: absolute;
+    width: 400px;
+    height: 500px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: #fff;
+    padding: 20px;
+    box-sizing: border-box;
+    border-radius: 8px;
+    
+}
+
+
+
+</style>
